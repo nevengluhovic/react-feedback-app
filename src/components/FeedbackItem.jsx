3d4e4a6807a0cd7b8e693daf68460d15aadf0ed8
Neven Gluhovic <nevengluhovic@gmail.com>
2022-01-26 20:53:49 +0100

    Feedback comp

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FeedbackItem = () => {
+  return (
+    <div className="card">
+      <div className="num-display">10</div>
+      <div className="text-display">This is an feedback</div>
+    </div>
+  );
+};
+
+export default FeedbackItem;
